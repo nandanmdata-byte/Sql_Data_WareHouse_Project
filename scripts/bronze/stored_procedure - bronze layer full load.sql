ffbdbd1d78e080eed6ece30ed77bdabccf8f9cc6
Nandan M <nandan.m.data@gmail.com>
2026-02-26 10:35:48 +0530

    modified stored_procedure - bronze layer full load

--- scripts/bronze/stored_procedure - bronze layer full load.sql
+++ scripts/bronze/stored_procedure - bronze layer full load.sql
@@ -42,7 +42,7 @@ BEGIN
 
 		PRINT '>> Loading data into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\SQL_User\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\Sql_Data_WareHouse_Project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -58,7 +58,7 @@ BEGIN
 
 		PRINT '>> Loading data into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\SQL_User\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\Sql_Data_WareHouse_Project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -74,7 +74,7 @@ BEGIN
 
 		PRINT '>> Loading data into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\SQL_User\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Sql_Data_WareHouse_Project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -96,7 +96,7 @@ BEGIN
 
 		PRINT '>> Loading data into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\SQL_User\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		FROM 'C:\Sql_Data_WareHouse_Project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -112,7 +112,7 @@ BEGIN
 
 		PRINT '>> Loading data into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\SQL_User\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\Sql_Data_WareHouse_Project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -128,7 +128,7 @@ BEGIN
 
 		PRINT '>> Loading data into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\SQL_User\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\Sql_Data_WareHouse_Project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
